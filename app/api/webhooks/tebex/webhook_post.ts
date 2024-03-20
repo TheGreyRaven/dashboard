8420b48ba0c9849ec93e2856ac23eb211198f5f4
TheGreyRaven <theravengrey@gmail.com>
2024-03-20 14:04:21 +0100

    Get json instead

--- app/api/webhooks/tebex/webhook_post.ts
+++ app/api/webhooks/tebex/webhook_post.ts
@@ -1,9 +1,21 @@
+import * as crypto from "crypto";
 import { NextRequest, NextResponse } from "next/server";
 
 const SECRET = process.env.TEBEX_SECRET;
 
 const POST = async (_req: NextRequest, _res: NextResponse) => {
-  console.log(_req.body);
+  const postData = await _req.json();
+  console.log(postData);
+
+  // const bodyHash = crypto
+  //   .createHash("sha256")
+  //   .update(_req.body, "utf-8")
+  //   .digest("hex");
+  // const finalHash = crypto
+  //   .createHmac("sha256", secret)
+  //   .update(bodyHash)
+  //   .digest("hex");
+  // console.log("finalHash", finalHash);
 
   return Response.json("ok");
 };
