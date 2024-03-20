da832e43589236b6d0c10f400c75f2eb207dd765
TheGreyRaven <theravengrey@gmail.com>
2024-03-20 14:11:07 +0100

    Return message hash

--- app/api/webhooks/tebex/webhook_post.ts
+++ app/api/webhooks/tebex/webhook_post.ts
@@ -1,25 +1,30 @@
 import * as crypto from "crypto";
 import { NextRequest, NextResponse } from "next/server";
 
-const SECRET = process.env.TEBEX_SECRET;
+const SECRET = process.env.TEBEX_SECRET ?? "";
 
 const POST = async (_req: NextRequest, _res: NextResponse) => {
   const postData = await _req.json();
   console.log(postData);
 
-  // const bodyHash = crypto
-  //   .createHash("sha256")
-  //   .update(_req.body, "utf-8")
-  //   .digest("hex");
-  // const finalHash = crypto
-  //   .createHmac("sha256", secret)
-  //   .update(bodyHash)
-  //   .digest("hex");
-  // console.log("finalHash", finalHash);
+  const bodyHash = crypto
+    .createHash("sha256")
+    .update(postData, "utf-8")
+    .digest("hex");
+  const finalHash = crypto
+    .createHmac("sha256", SECRET)
+    .update(bodyHash)
+    .digest("hex");
+  console.log("finalHash", finalHash);
 
-  return Response.json("ok", {
-    status: 200,
-  });
+  return Response.json(
+    {
+      id: finalHash,
+    },
+    {
+      status: 200,
+    }
+  );
 };
 
 export { POST };
