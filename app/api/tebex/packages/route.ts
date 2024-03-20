6abb5a9a5a990e60f2b7de27c386a9c883f7d518
TheGreyRaven <theravengrey@gmail.com>
2024-03-20 17:12:48 +0100

    Added error message on failure

--- app/api/tebex/packages/route.ts
+++ app/api/tebex/packages/route.ts
@@ -3,17 +3,30 @@ import { NextRequest, NextResponse } from "next/server";
 const TEBEX_URL = `https://headless.tebex.io/api/accounts/${process.env.TEBEX_PUBLIC_KEY}/categories?includePackages=1`;
 
 const GET = async (_req: NextRequest, _res: NextResponse) => {
-  const tebexRaw = await fetch(TEBEX_URL, {
-    headers: {
-      Accept: "application/json",
-    },
-  });
+  try {
+    const tebexRaw = await fetch(TEBEX_URL, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
 
-  const { data } = await tebexRaw.json();
+    const { data } = await tebexRaw.json();
 
-  return Response.json(data, {
-    status: 200,
-  });
+    return Response.json(data, {
+      status: 200,
+    });
+  } catch (err: any) {
+    console.error(err);
+
+    return Response.json(
+      {
+        message: "Failed to fetch Tebex packages",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 };
 
 export { GET };
